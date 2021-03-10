d78bf43a56fe31b3994c2882c1722f530eb0db7a
TheoXiong7 <68202458+TheoXiong7@users.noreply.github.com>
2021-03-10 15:59:07 -0500

    init

---
+++ mwdictionary.py
@@ -0,0 +1,35 @@
+# Theo Xiong 2021/3/10
+# Wrapper for the Merriam-Webster dictionary API
+
+import requests
+import json
+
+def define(word, key):
+	if not word or word.isspace():
+		res = {
+		'word': 'null',
+		'definition': 'null',
+		'type': 'null',
+		'offensive': 'null'
+		}
+		return res
+	url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
+	geturl = url + word + '?key=' + key
+	req = requests.get(geturl)
+	req_text = json.loads(req.text)
+	try:
+		res = {
+		'word': word,
+		'definition': req_text[0]['shortdef'][0],
+		'type': req_text[0]['fl'],
+		'offensive': str(req_text[0]['meta']['offensive'])
+		}
+	except:
+		res = {
+		'word': word,
+		'definition': 'null',
+		'type': 'null',
+		'offensive': 'null'
+		}
+	return res
+
